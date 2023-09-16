111fea364d97d0d5837b8e9f539b87ac0d7b7ddd
Enrique Romero <enrique.romero1110@gmail.com>
2023-09-16 16:01:23 -0400

    Fixed naming of input box container

--- app/components/InputBox.tsx
+++ app/components/InputBox.tsx
@@ -2,11 +2,11 @@ import React from 'react'
 import Calculate from './Calculate'
 import styles from './InputBox.module.css'
 
-styles.inputBox
+styles.inputBoxContainer
 
 const InputBox = () => {
   return (
-    <div className={styles.inputBox}>
+    <div className={styles.inputBoxContainer}>
         <Calculate />
     </div>
   )

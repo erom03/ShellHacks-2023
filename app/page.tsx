b6312f9700f89588740251fed0fabe9054ad746b
Enrique Romero <enrique.romero1110@gmail.com>
2023-09-16 15:51:53 -0400

    Add field for input box

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
 import Image from 'next/image'
+import InputBox from './components/InputBox'
 
 export default function Home() {
   return (
-    <h1>Hello World!</h1>
+    <main>
+      <h1>Hello World!</h1>
+      <InputBox />
+    </main>
   )
 }

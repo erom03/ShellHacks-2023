1eab33fa4136ce6ea8992e574cea8187ff5e28da
Enrique Romero <enrique.romero1110@gmail.com>
2023-09-16 15:51:01 -0400

    Add calculate button

---
+++ app/components/Calculate.tsx
@@ -0,0 +1,12 @@
+'use client';
+import React from 'react'
+
+const Calculate = () => {
+  return (
+    <div>
+        <button onClick={() => console.log('working')}>Calculate</button>
+    </div>
+  )
+}
+
+export default Calculate

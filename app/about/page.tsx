cc923ec4e0255c7ed43325f18abdf470ed92570b
Enrique Romero <enrique.romero1110@gmail.com>
2023-09-17 04:43:37 -0400

    Fixed potential issue with apostrophese stopping the github pages build from working

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -21,7 +21,7 @@ const page = () => {
     <br/>
     <div className="bg-sky-500 p-5 text-white font-normal shadow-xl rounded-md personOne w-5/3 mx-5 my-5 text-wrap">
         <label>
-        20% Down Payment: Start by saving up at least 20% of the car's total cost for a down payment. This 
+        20% Down Payment: Start by saving up at least 20% of the car&apos;s total cost for a down payment. This 
         not only reduces the amount you need to finance but also decreases the potential for the dealership to 
         capitalize on high-interest loans, as a larger down payment often leads to better loan terms.
         </label>
@@ -29,14 +29,14 @@ const page = () => {
     <div className="bg-sky-500 p-5 text-white font-normal shadow-xl rounded-md personOne w-5/3 mx-5 my-5">
         <label>
         4-Year Loan Term: Limit your car loan term to no more than 4 years (48 months). This minimizes the 
-        amount of interest you'll pay over the life of the loan, making it less lucrative for the dealership 
+        amount of interest you&apos;ll pay over the life of the loan, making it less lucrative for the dealership 
         to push longer loan terms with higher interest rates.
         </label>
     </div>
     <div className="bg-sky-500 p-5 text-white font-normal shadow-xl rounded-md personOne w-5/3 mx-5 my-5">
         <label>
         10% or Less of Your Monthly Income: Ensure that your monthly car payment, including insurance, fuel, 
-        and maintenance, doesn't exceed 10% of your monthly income. By sticking to this guideline, you safeguard 
+        and maintenance, doesn&apos;t exceed 10% of your monthly income. By sticking to this guideline, you safeguard 
         yourself from being enticed into unaffordable financing arrangements that may benefit the dealership more than you.
         </label>
     </div>

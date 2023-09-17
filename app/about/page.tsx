87349ea8e09fc8e6ca80b3cd598a3cbf184afa38
Enrique Romero <enrique.romero1110@gmail.com>
2023-09-17 00:48:12 -0400

    I got some stuff done...

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -5,9 +5,11 @@ const page = () => {
   return (
     <>
     <header><Navigation /></header>
-    <div className='content-center'>
-        <h1>The 20/4/10 Rule</h1>
-    </div>
+    <main>
+        <div>
+            <h1>The 20/4/10 Rule</h1>
+        </div>
+    </main>
     </>
   )
 }

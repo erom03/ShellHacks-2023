47d37f4cdcfd8d7bf144cfeb6b24309eba454db8
Enrique Romero <enrique.romero1110@gmail.com>
2023-09-16 20:48:57 -0400

    Added CSS

--- app/components/InputBox.tsx
+++ app/components/InputBox.tsx
@@ -1,19 +1,20 @@
 "use client";
 import React, { useState } from 'react';
 import Calculate from './CalculateButton'
+import styles from './InputBox.module.css'
 
 const InputBox = () => {
   
-  const [salary, SetSalary] = useState("");   // Need salary
+  const [salary, SetSalary] = useState("");
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
-    const blog = { salary };  // Not working need salary from InputField
+    const blog = { salary };
 
     console.log(blog);
   }
   
   return (
-    <div className="bg-sky-500 p-5 text-black font-normal shadow-xl rounded-md static">
+    <div className="bg-sky-500 p-5 text-black font-normal shadow-xl rounded-md inputBoxContainer">
       <form onSubmit={handleSubmit}>
 
       <div>

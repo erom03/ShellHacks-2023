4e5272f46e7f2aff18a72cc8b21f684ad0284492
Enrique Romero <enrique.romero1110@gmail.com>
2023-09-17 04:45:36 -0400

    THESE DARN APOSTROPHESE

--- app/components/Calculation.tsx
+++ app/components/Calculation.tsx
@@ -10,7 +10,7 @@ const AffordAbleAmount = ({IsValueEntered, MonthlyPayment}: ICalculationsProp) =
     return (
         <label>
             {Math.floor(MonthlyPayment) > 173 && IsValueEntered && <label className="text-white font-light py-10">  Maximum Monthly Payment: { Math.floor(MonthlyPayment) } </label>}
-            {Math.floor(MonthlyPayment) < 173 && IsValueEntered && <label>Sorry that's too low to affoard a car.</label>}
+            {Math.floor(MonthlyPayment) < 173 && IsValueEntered && <label>Sorry that&apos;s too low to affoard a car.</label>}
         </label>
     )
 }

e454e488a958d370177d3513d80a65bf1cd04beb
Enrique Romero <enrique.romero1110@gmail.com>
2023-09-17 00:19:53 -0400

    Add about page

---
+++ app/about/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navigation from '../components/navigation'
+
+const page = () => {
+  return (
+    <>
+    <header><Navigation /></header>
+    <div className='content-center'>
+        <h1>The 20/4/10 Rule</h1>
+    </div>
+    </>
+  )
+}
+
+export default page

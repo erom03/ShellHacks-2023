f8ed400f8b9b85b6d350cd743b09cbcc0b9b85d7
Enrique Romero <enrique.romero1110@gmail.com>
2023-09-16 23:13:39 -0400

    Fix color add shadow

--- app/components/navigation/index.tsx
+++ app/components/navigation/index.tsx
@@ -6,19 +6,19 @@ import Button from "./Button";
 const Navbar = () => {
   return (
     <>
-      <div className="w-full h-20 bg-emerald-800 sticky top-0">
+      <div className="w-full h-20 bg-sky-500 sticky top-0 shadow-md">
         <div className="container mx-auto px-4 h-full">
           <div className="flex justify-between items-center h-full">
             <Logo />
             <ul className="hidden md:flex gap-x-6 text-white">
-              <li>
-                <Link href="/about">
-                  <p>About Us</p>
+            <li>
+                <Link href="/">
+                  <p>Home</p>
                 </Link>
               </li>
               <li>
-                <Link href="/services">
-                  <p>Services</p>
+                <Link href="/about">
+                  <p>About Us</p>
                 </Link>
               </li>
               <li>
